f03b9756c471cdf175cb04a8f6dca0eec3745b96
MonyVannn <2022147men@aupp.edu.kh>
2022-04-03 11:33:01 +0700

    starting file

--- homework_6_exercises4_menmonyvann.py
+++ homework_6_exercises4_menmonyvann.py
@@ -1,4 +1,25 @@
-print("hello world")
+# EXERCISE 1
+def replace_last():
+    pass
 
-def sayhi(name):
-    print(f"hi {name}")
+# EXERCISE 2
+def index_power():
+    pass
+
+# EXERCISE 3
+def remove_all_after():
+    pass
+
+# EXERCISE 4
+def chunking_by():
+    pass
+
+# EXERCISE 5
+def backward_string_by_word():
+    pass
+
+def init():
+    pass
+
+if __name__ == "__main__":
+    init()
